d28fa3486304564955e2641e9e8c1de0a608ffcf
Jomix-13 <80495274+Jomix-13@users.noreply.github.com>
2021-08-25 11:50:48 -0700

    Home page

---
+++ react-app/src/components/googlemap/index.js
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+
+function Map () {
+    
+    // const AnyReactComponent = ({ text }) => <div>{text}</div>;
+    
+    const bayarea = {
+        center: {
+        lat: 37.8272,
+        lng: -122.2913
+        },
+        zoom: 11
+    };
+
+    return (
+      // Important! Always set the container height explicitly
+      <div className='googleMap'>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBUDoNSCh1np3t8RCyZ-BZRkbjMFb_9Gyg' }}
+          defaultCenter={bayarea.center}
+          defaultZoom={bayarea.zoom}
+        >
+          {/* <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          /> */}
+        </GoogleMapReact>
+      </div>
+    );
+  
+}
+
+export default Map;
